84a0833b543a68c6f9803b7d9c28bcd1fb796c66
Jenny Tellne <jenny.tellne@gmail.com>
2018-01-10 14:18:50 +0100

    Map with geolocation

--- new/script.js
+++ new/script.js
@@ -15,95 +15,34 @@ function openPost(evt, postName) {
 
 
 /* MAP CODE */
-//Created this function to try to make the map work but we should use the code under in some way
-//If using another function than myMap(), update this part in the <script> in the htmlfile as it contains
-//the function in the link
 function myMap() {
-    var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.12),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-    }
-    var map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
-}
 
-//This is the code from the course website 
-/**************************
-//Script for the google map
-**************************/
+if(!!navigator.geolocation) {
+        var map;
+        var mapOptions = {
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
 
-/*var map = null;
-var geocoder = null;
-var googleMap = document.getElementById('google-map');
-
-function showPosition(position) {
-//Fonction to be passed to the browser's geolocation function
-    var latLng = new google.maps.LatLng(
-        position.coords.latitude, position.coords.longitude);
-    var marker = new google.maps.Marker({
-      position: latLng,
-      map: map
-    });
-    map.setCenter(latLng);
-    map.setZoom(15);
-  }
-
-  function showMap() {
-    //This creates the initial map, or update it
-    if (!map) {
-      map = new google.maps.Map(googleMap, {
-        zoom: 3,
-        center: new google.maps.LatLng(37.4419, -94.1419),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      });
-    }
-    if(!geocoder){
-      geocoder = new google.maps.Geocoder();
-    }
-
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    }
-  }
-
-  function updateMap(){
-    var newLocation = document.getElementById("location-box").value;
-    geocoder.geocode( { 'address': newLocation}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location, 8);
-        var marker = new google.maps.Marker({
-        map: map, 
-        position: results[0].geometry.location
+        map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+        navigator.geolocation.getCurrentPosition(function(position) {
+  
+            var geolocate = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            var infowindow = new google.maps.InfoWindow({
+                map: map,
+                position: geolocate,
+                content:
+                    '<h1>Location pinned from HTML5 Geolocation!</h1>' +
+                    '<h2>Latitude: ' + position.coords.latitude + '</h2>' +
+                    '<h2>Longitude: ' + position.coords.longitude + '</h2>'
+            });
+            map.setCenter(geolocate);
         });
-      } else {
-        alert("Error: " + status);
-      }
-    });
-  }
-  
-  var map = new google.maps.Map(document.getElementById("google-map"), map);
-  //Clicking on the "Show the map" will trigger the showMap() function
-  document.getElementById('google-map').addEventListener('click',showMap, true);
-  //Clicking on the update button will...update (the map)
-  document.getElementById('update-map').addEventListener('click',updateMap, true);
-  
-  //For some reason, this doesn't work as expected : 
-  //pressing "enter" when focused on the location field should return false...but I had to hard-code it in the HTML
-  document.getElementById('geolocation-form').addEventListener('submit',function(){return false;},true);
-*/
-/*
-function jukeBox{
-  var video = document.createElement("video");
-  var source = document.createElement("source"); 
-  source.type = "video/ogg";
-  var videos = ["https://upload.wikimedia.org/wikipedia/commons/transcoded/3/36/History_of_the_Lake_Eola_Fountain.webm/History_of_the_Lake_Eola_Fountain.webm.240p.webm", "https://upload.wikimedia.org/wikipedia/commons/transcoded/6/60/SunRail_Grand_Opening_at_Church_Street.webm/SunRail_Grand_Opening_at_Church_Street.webm.480p.webm", "https://upload.wikimedia.org/wikipedia/commons/transcoded/8/83/SunRail_Grand_Opening_at_Florida_Hospital.webm/SunRail_Grand_Opening_at_Florida_Hospital.webm.480p.webm"];
-  for(int i = 0; i < videos.length; i++){
-    source.src = videos[i];
-    video.appendChild(source);
-    video.load();
-    video.play();
-  }
-} */
+        
+    } else {
+        document.getElementById('google-maps').innerHTML = 'No Geolocation Support.';
+    }
+};
 
 
 var videos = ["https://upload.wikimedia.org/wikipedia/commons/transcoded/3/36/History_of_the_Lake_Eola_Fountain.webm/History_of_the_Lake_Eola_Fountain.webm.240p.webm", "https://upload.wikimedia.org/wikipedia/commons/transcoded/6/60/SunRail_Grand_Opening_at_Church_Street.webm/SunRail_Grand_Opening_at_Church_Street.webm.480p.webm", "https://upload.wikimedia.org/wikipedia/commons/transcoded/8/83/SunRail_Grand_Opening_at_Florida_Hospital.webm/SunRail_Grand_Opening_at_Florida_Hospital.webm.480p.webm"];
